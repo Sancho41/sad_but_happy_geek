cd00e548ab042d1ba6347e5f8807f6d2c57b356e
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-08-08 13:37:14 -0300

    feat: Created vies Happiness.vue

---
+++ src/views/Happiness.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="happiness">
+    <h1 class="title">- I'm so sad!</h1>
+    <button class="button-title" @click="openModal">Tell a joke to the Geek!</button>
+    <Modal ref="modal" />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+import Modal from "../components/Modal";
+
+@Component({
+  components: {
+    Modal,
+  },
+})
+export default class Happiness extends Vue {
+  created() {
+    this.$store.dispatch("makeItSad");
+  }
+
+  openModal() {
+    this.$refs.modal.openModal();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#happiness {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
