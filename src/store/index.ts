6b04266e6e2350e84f70d6dadbe617c97fd4deaa
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-08-08 13:38:01 -0300

    feat: Created state manager

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,41 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    happiness: 50,
+    openModal: false,
   },
+
   mutations: {
+    setHappiness(state, amount) {
+      if (state.happiness < 100) state.happiness = amount;
+    },
+    makeItSad(state) {
+      state.happiness = 0;
+    },
+    makeItNeutral(state) {
+      state.happiness = 50;
+    },
+    switchModal(state, open) {
+      state.openModal = open;
+    },
   },
+
   actions: {
+    setHappiness(context, amount) {
+      context.commit("setHappiness", amount);
+    },
+    makeItSad(context) {
+      context.commit("makeItSad");
+    },
+    makeItNeutral(context) {
+      context.commit("makeItNeutral");
+    },
+    switchModal(context, open) {
+      context.commit("switchModal", open);
+    },
   },
-  modules: {
-  }
-})
+});
